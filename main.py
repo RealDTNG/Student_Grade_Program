3d2406e2ecd0da9d06fb9e4b9f4439bdaacc85f6
DawsonH <ddwh2005@gmail.com>
2023-02-22 11:04:16 -0500

    list code

--- main.py
+++ main.py
@@ -3,14 +3,21 @@
 # Student grade program
 
 import time, os
-Student_list = [[]]
+Student_list = [["Dylan","Baker", 98, 90, 92, 93],["Dave","Johnson",67,86,74,93],["Jacob","Kakowski",73,59,64,83],["JOsh","Hotter",51,57,53,99],["Erik","Lagsaway",100,100,100,100]]
     
     
 def add():
     print("adding students")
     
 def list():
-    print("viewing list")
+    m_leng = 0
+    for firstn, lastn, gr1, gr2, gr3, gr4 in Student_list:
+        if len(firstn + lastn)> m_leng:
+            m_leng = len(firstn + lastn)
+    for firstn, lastn, gr1, gr2, gr3, gr4 in Student_list:
+        print(f"{lastn}, {firstn}:{' '*(m_leng + 5 - len(firstn + lastn))}{gr1} {gr2} {gr3} {gr4}")
+    time.sleep(1)
+    input("To Return To Menu Press [Enter]")
     
 def Student_av():
     print("student averages")
@@ -25,16 +32,16 @@ def exit_G():
 
 menus = {"1":add, "2":list, "3":Student_av, "4": Course_av, "5":exit_G}
 while True:
-    print("1 | To Add Or Remove Students")
-    print("2 | To View The List")
-    print("3 | To View The Student Averages")
-    print("4 | To View The Course Average")
-    print("5 | To Exit")
+    os.system('cls')
+    print(" '1' | To Add Students")
+    print(" '2' | To View The List")
+    print(" '3' | To View The Student Averages")
+    print(" '4' | To View The Course Average")
+    print(" '5' | To Exit")
     choice = input(f"Please pick a menu option\n\n:")
     if choice in menus:
         os.system('cls')
         menus[choice]()
-        break
     else:
         os.system('cls')
         print("ERROR, Please enter a valid choice")
