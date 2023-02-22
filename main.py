a6271139aea28f1f3677b6cb3243b6b65048d997
DawsonH <ddwh2005@gmail.com>
2023-02-22 10:23:15 -0500

    IT WORRRKS

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+# Dawson Hoyle
+# Feb 21 2021
+# Student grade program
 
+import time, os
 Student_list = [[]]
     
     
@@ -18,20 +22,20 @@ def exit_G():
     print("See you next time")
     exit()
     
-def menu():
-    menus = {0:add, 1:list, 2:Student_av, 3: Course_av, 4:exit_G}
-    while True:
-        print("1 | To Add Or Remove Students")
-        print("2 | To View The List")
-        print("3 | To View The Student Averages")
-        print("4 | To View The Course Average")
-        print("5 | To Exit")
-        choice = int(input(f"Please pick a menu option\n\n:"))-1
-        #while choice not in menus:
-        #    print("ERROR, Please enter a valid choice")
-        #    choice = input(f"Please pick a menu option\n\n:")
-        menus[choice]
 
-
-
-menu()
+menus = {"1":add, "2":list, "3":Student_av, "4": Course_av, "5":exit_G}
+while True:
+    print("1 | To Add Or Remove Students")
+    print("2 | To View The List")
+    print("3 | To View The Student Averages")
+    print("4 | To View The Course Average")
+    print("5 | To Exit")
+    choice = input(f"Please pick a menu option\n\n:")
+    if choice in menus:
+        os.system('cls')
+        menus[choice]()
+        break
+    else:
+        os.system('cls')
+        print("ERROR, Please enter a valid choice")
+        time.sleep(1)
